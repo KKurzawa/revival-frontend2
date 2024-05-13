bb3fdd087276f65f1ef4c44a18927ab7c5d2547e
Kris Kurzawa <kurzawa1@yahoo.com>
2024-05-12 20:13:36 -0400

    added contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,86 @@
 import './Contact.css'
+import { validateEmail } from '../../utils/EmailValidater.js'
+import emailjs from '@emailjs/browser'
+import { useState } from 'react'
 
 const Contact = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!validateEmail(email)) {
+            alert('Please Enter A Valid Email');
+            return;
+        }
+
+        const serviceId = 'service_7z3rq0t';
+        const templateId = 'template_cq7z9hc';
+        const publicKey = 'B2PCc5s-Jybuu2Tck';
+
+        const templateParams = {
+            from_name: name,
+            subject: subject,
+            message: message,
+        };
+
+        emailjs.send(serviceId, templateId, templateParams, publicKey)
+            .then((response) => {
+                console.log('Email sent successfully!', response);
+                alert(`Thank you for your message ${name}!  We will get back to you soon!`);
+                setName('');
+                setEmail('');
+                setSubject('');
+                setMessage('');
+            })
+            .catch((error) => {
+                console.error('Error:', error)
+            })
+    }
     return (
         <main id='contact' className='flex flex-col items-center text-[whitesmoke]'>
             <header className='contact-header py-2 md:py-5 w-full text-center'><span className='text-[2rem]'>C</span><span className='text-3xl'>ONTAC</span><span className='text-[2rem]'>T</span></header>
+            <article className='email-form-container flex flex-col items-center w-full'>
+                <form onSubmit={handleSubmit} className='text-[#f5f5f5] flex flex-col content-center w-[80%] lg:w-[50%] my-10'>
+                    <article className='contact-form flex flex-col rounded-xl'>
+                        <input
+                            className='user-name outline-none h-10 px-2 my-1 text-xl bg-transparent rounded-xl'
+                            type='text'
+                            placeholder='Your Name'
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                        <input
+                            className='user-email outline-none h-10 px-2 my-1 text-xl bg-transparent rounded-xl'
+                            type='email'
+                            placeholder='Your Email'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <input
+                            className='user-subject outline-none h-10 px-2 my-1 text-xl bg-transparent rounded-xl'
+                            type='subject'
+                            placeholder='Subject'
+                            value={subject}
+                            onChange={(e) => setSubject(e.target.value)}
+                        />
+                        <textarea
+                            className='user-message outline-none px-2 py-2 my-1 text-xl bg-transparent rounded-xl'
+                            cols='30'
+                            rows='5'
+                            placeholder='Message'
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                        >
+                        </textarea>
+                    </article>
+                    <button className='post-button w-fit p-3 mt-1 rounded-xl'>Submit</button>
+                </form>
+                <a href='/#top' className='contact-top-link flex justify-center py-2 w-full text-xl'>To the Top</a>
+            </article>
         </main>
     )
 }
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -66,22 +66,22 @@ const Testimonials = () => {
 
     return (
         <main id='testimonials' className='flex flex-col items-center text-[whitesmoke]'>
-            <header className='testimonials-header py-2 md:py-5 w-full text-center'><span className='text-[2rem]'>T</span><span className='text-3xl'>ESTIMONIAL</span><span className='text-[2rem]'>S</span></header>
+            <header className='testimonials-header py-2 md:py-5 w-full text-center'><span className='text-[1.75rem] md:text-[2rem]'>T</span><span className='text-[1.55rem] md:text-3xl'>ESTIMONIAL</span><span className='text-[1.75rem] md:text-[2rem]'>S</span></header>
             <article className="comments-container text-[whitesmoke]">
                 {comments.map((item) => (
-                    <section key={item.comment} className="comment-name px-14 lg:px-20 py-10">
-                        <h2 className="text-center text-3xl pb-2">{item.comment}</h2>
-                        <h2 className="text-end text-xl italic">- {item.userfirstname}&nbsp;{item.userlastname}</h2>
+                    <section key={item.comment} className="comment-name px-5 lg:px-20 py-10">
+                        <h2 className="text-center text-xl md:text-3xl pb-2">{item.comment}</h2>
+                        <h2 className="text-end text-base md:text-xl italic">- {item.userfirstname}&nbsp;{item.userlastname}</h2>
                     </section>))}
             </article>
-            <header className='testimonials-header py-2 md:py-5 w-full text-center'><span className='text-[2rem]'>S</span><span className='text-3xl'>HAR</span><span className='text-[2rem]'>E</span><span className='text-[2rem]'>  Y</span><span className='text-3xl'>OU</span><span className='text-[2rem]'>R</span><span className='text-[2rem]'>  R</span><span className='text-3xl'>EVIVA</span><span className='text-[2rem]'>L</span><span className='text-[2rem]'>  E</span><span className='text-3xl'>XPERIENC</span><span className='text-[2rem]'>E</span></header>
+            <header className='testimonials-header py-2 md:py-5 w-full text-center'><span className='text-[1.75rem] md:text-[2rem]'>Y</span><span className='text-[1.55rem] md:text-3xl'>OU</span><span className='text-[1.75rem] md:text-[2rem]'>R</span><span className='text-[1.75rem] md:text-[2rem]'>  E</span><span className='text-[1.55rem] md:text-3xl'>XPERIENC</span><span className='text-[1.75rem] md:text-[2rem]'>E</span></header>
             <article className='form-container flex flex-col items-center w-full'>
-                <form onSubmit={handleSubmit} className="text-[#f5f5f5] flex flex-col content-center w-[50%] my-10">
+                <form onSubmit={handleSubmit} className="text-[#f5f5f5] flex flex-col content-center w-[80%] lg:w-[50%] my-10">
                     <article className='comments-form flex flex-col rounded-xl'>
                         <input onChange={handleChange}
                             name="userfirstname" value={input.userfirstname}
                             autoComplete="off"
-                            className="user-first-name outline-none h-10 px-2 text-xl bg-transparent"
+                            className="user-first-name outline-none h-10 px-2 my-1 text-xl bg-transparent rounded-xl"
                             placeholder="Your First Name"
                             type="comment"
                         >
@@ -89,7 +89,7 @@ const Testimonials = () => {
                         <input onChange={handleChange}
                             name="userlastname" value={input.userlastname}
                             autoComplete="off"
-                            className="user-last-name outline-none h-10 px-2 text-xl bg-transparent"
+                            className="user-last-name outline-none h-10 px-2 my-1 text-xl bg-transparent rounded-xl"
                             placeholder="Your Last Name"
                             type="comment"
                         >
@@ -100,14 +100,14 @@ const Testimonials = () => {
                             value={input.comment}
                             autoComplete="off"
                             type="comment"
-                            className="user-experience outline-none px-2 py-2 text-xl bg-transparent"
+                            className="user-experience outline-none px-2 py-2 my-1 text-xl bg-transparent rounded-xl"
                             placeholder="Your Experience"
                             cols='20'
                             rows='5'
                         >
                         </textarea>
                     </article>
-                    <button className="post-button w-fit p-3 mt-5 rounded-xl">Add Post</button>
+                    <button className="post-button w-fit p-3 mt-1 rounded-xl">Add Post</button>
                 </form>
                 <a href='/#top' className='testimonials-top-link flex justify-center py-2 w-full text-xl'>To the Top</a>
             </article>
