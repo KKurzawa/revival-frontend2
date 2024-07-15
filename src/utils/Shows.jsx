0f75f3e6160b185c85ce0d56b898551fb2574ff5
Kris Kurzawa <kurzawa1@yahoo.com>
2024-07-15 12:44:24 -0400

    updated shows

--- src/utils/Shows.jsx
+++ src/utils/Shows.jsx
@@ -1,13 +1,6 @@
 export const shows = [
     {
-        date: 'June 22 2024 4:30pm',
-        venue: 'Brightons Yellowstone Music Festival',
-        location: 'Brighton, MI',
-        venueLink: 'https://www.brightoncoc.org/brightons-yellowstone-country-music-festival/',
-        ticketLink: 'https://www.eventbrite.com/e/brightons-2nd-annual-yellowstone-country-music-festival-tickets-876814053437'
-    },
-    {
-        date: 'July 25 2024 7pm',
+        date: 'September 5 2024 7pm',
         venue: "The LaFontaine Family Amphitheater",
         location: 'Milford, MI',
         venueLink: "https://www.meetmeinmilford.com/downtown-events/summer-concert-series/",
@@ -29,10 +22,31 @@ export const shows = [
         ticketLink: 2
     },
     {
+        date: 'September 5 2024 7pm',
+        venue: "The LaFontaine Family Amphitheater",
+        location: 'Milford, MI',
+        venueLink: "https://www.meetmeinmilford.com/downtown-events/summer-concert-series/",
+        ticketLink: 1
+    },
+    {
         date: 'October 5 2024 7pm',
         venue: 'Wildwood Ampetheater',
         location: 'Lake Orion, MI',
         venueLink: 'https://www.orionparks.com/facilities/parks_and_trails/wildwood.php',
+        ticketLink: 2
+    },
+    {
+        date: 'January 17 2024 8:30pm',
+        venue: 'The Cadieux Cafe',
+        location: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com',
+        ticketLink: 2
+    },
+    {
+        date: 'January 18 2024 8:30pm',
+        venue: 'The Cadieux Cafe',
+        location: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com',
         ticketLink: 2
     },
 ]
