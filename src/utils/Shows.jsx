f702e75e1b12e47ba498a8c412f85279e1ca1909
Kris Kurzawa <kurzawa1@yahoo.com>
2024-08-20 22:51:07 -0400

    updated shows

--- src/utils/Shows.jsx
+++ src/utils/Shows.jsx
@@ -1,20 +1,5 @@
 export const shows = [
     {
-        date: 'August 8 2024 7pm',
-        time: '7pm',
-        venue: "Centenial Commons Park",
-        location: 'Royal Oak, MI',
-        venueLink: "https://www.royaloakchamber.com/summer-concert-series",
-        ticketLink: 1
-    },
-    {
-        date: 'August 16 2024 7:30pm',
-        venue: 'The Cadieux Cafe',
-        location: 'Detroit, MI',
-        venueLink: 'https://www.cadieuxcafe.com',
-        ticketLink: 2
-    },
-    {
         date: 'September 5 2024 7pm',
         venue: "The LaFontaine Family Amphitheater",
         location: 'Milford, MI',
