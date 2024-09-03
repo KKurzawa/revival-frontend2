2c7d2c63797c8067f9b315b7b0e8b2ac6dbc8dac
Kris Kurzawa <kurzawa1@yahoo.com>
2024-09-03 14:30:31 -0400

    removed alt attribute

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,8 +5,8 @@ const Landing = () => {
     return (
         <main className='landing-main flex h-full w-full'>
             <article className='flex flex-col justify-center items-center h-screen w-full'>
-                <img src={logo} alt='logo' className='mobile-landing-logo flex md:hidden w-[90%] h-auto' />
-                <img src={logo} alt='logo' className='landing-logo hidden md:flex h-auto' />
+                <img src={logo} alt='' className='mobile-landing-logo flex md:hidden w-[90%] h-auto' />
+                <img src={logo} alt='' className='landing-logo hidden md:flex h-auto' />
                 <h1 className='landing-text text-center pt-3 text-lg md:text-2xl text-[whitesmoke]'>Detroits <span className='ultimate'>Ultimate </span><span className='allman'>Allman </span><span className='brothers'>Brothers </span><span className='experience'>Experience</span></h1>
             </article>
 
