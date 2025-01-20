5898bcd341e62479ae08c40ea60ea8dc57d4729f
Kris Kurzawa <kurzawa1@yahoo.com>
2025-01-20 12:24:31 -0500

    updated shows

--- src/utils/Shows.jsx
+++ src/utils/Shows.jsx
@@ -1,20 +1,62 @@
 export const shows = [
     {
-        date: 'October 5 2024 7pm',
-        venue: 'Wildwood Ampetheater',
-        location: 'Lake Orion, MI',
-        venueLink: 'https://www.orionparks.com/facilities/parks_and_trails/wildwood.php',
-        ticketLink: 2
+        date: 'January 31 2025, 7pm',
+        venue: 'Raue Center for the Arts',
+        location: 'Crystal Lake, IL',
+        venueLink: 'https://www.rauecenter.org',
+        ticketLink: 'https://www.etix.com/ticket/p/67561393/the-revival-brothers-banda-tribute-to-the-allman-brothers-crystal-lake-raue-center-for-the-arts?partner_id=100'
     },
     {
-        date: 'January 17 2024 8:30pm',
+        date: 'April 19 2025 8:30pm',
         venue: 'The Cadieux Cafe',
         location: 'Detroit, MI',
         venueLink: 'https://www.cadieuxcafe.com',
         ticketLink: 2
     },
     {
-        date: 'January 18 2024 8:30pm',
+        date: 'May 9 2025 7:30pm',
+        venue: 'The Cadieux Cafe',
+        location: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com',
+        ticketLink: 2
+    },
+    {
+        date: 'June 13 2025 7:30pm',
+        venue: 'The Cadieux Cafe',
+        location: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com',
+        ticketLink: 2
+    },
+    {
+        date: 'July 3 2025 7pm',
+        venue: 'Lafontaine Family Amphitheate',
+        location: 'Milford, MI',
+        venueLink: 'https://www.villageofmilford.org/discover/parks/Central-Park/discover/parks/lafontaine_family_amphitheater.php',
+        ticketLink: 1
+    },
+    {
+        date: 'July 11 2025 7:30pm',
+        venue: 'The Cadieux Cafe',
+        location: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com',
+        ticketLink: 2
+    },
+    {
+        date: 'August 8 2025 7:30pm',
+        venue: 'The Cadieux Cafe',
+        location: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com',
+        ticketLink: 2
+    },
+    {
+        date: 'September 12 2025 7:30pm',
+        venue: 'The Cadieux Cafe',
+        location: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com',
+        ticketLink: 2
+    },
+    {
+        date: 'October 10 2025 7:30pm',
         venue: 'The Cadieux Cafe',
         location: 'Detroit, MI',
         venueLink: 'https://www.cadieuxcafe.com',
